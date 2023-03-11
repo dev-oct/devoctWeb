b30bf8e214066966e5edebce2868860e2570c16e
chintan0513 <chintantripathi1310@gmail.com>
2023-03-12 03:01:32 +0530

    initial setup with logo and color scheme

---
+++ website/src/App.js
@@ -0,0 +1,13 @@
+//import Signup from './components/Signup';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Home Page</h1>
+      {/* <Signup />*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ website/src/components/Signup.js
@@ -0,0 +1,9 @@
+const Signup = () => {
+    return (
+        <div>
+            <p>Sign Up Page</p>
+        </div>
+    );
+}
+
+export default Signup;
