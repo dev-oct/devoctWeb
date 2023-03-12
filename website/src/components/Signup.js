36af3a4536133e72ff924ac2c1db84a5e0f34ed9
chintan0513 <chintantripathi1310@gmail.com>
2023-03-12 21:47:35 +0530

    signup page done

--- website/src/components/Signup.js
+++ website/src/components/Signup.js
@@ -2,42 +2,54 @@ import '../styles/signup.css'
 import logo from '../assets/logo.png'
 import vector from '../assets/Vector.png'
 import { AiFillEye } from 'react-icons/ai';
+import { FaFacebookF } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import { BsLinkedin } from 'react-icons/bs';
 
 const Signup = () => {
     return (
         <main className="signup flex flex-row justify-between">
-            <div className="left flex flex-col py-2 px-16">
+            <div className="left flex flex-col py-0 px-16">
                 <div className="head px-11">
                     <img src={logo} alt="logo" className="logo h-36 w-36" />
                 </div>
-                <div className="body flex-col pt-8 mx-auto w-46">
-                    <h1 className="font-bold text-3xl">Create Account</h1>
-                    <div className="sform flex flex-col p-2">
-                        <input type="text" name="fname" className="fname bg-[#F1F1F1] px-4 py-1 placeholder:font-medium placeholder:text-gray-600 rounded-md m-2 focus:outline-none focus:ring focus:ring-[#56A6B1]" placeholder="Full Name" />
-                        <input type="text" name="mail" className="mail bg-[#F1F1F1] px-4 py-1 placeholder:font-medium placeholder:text-gray-600  rounded-md m-2 focus:outline-none focus:ring focus:ring-[#56A6B1]" placeholder="Email Address" />
+                <div className="body flex-col mt-5 mx-auto w-46">
+                    <h1 className="font-bold text-4xl">Create Account</h1>
+                    <div className="sform flex mt-2 flex-col p-2">
+                        <input type="text" name="fname" className="fname bg-[#F1F1F1] px-4 py-1 w-96 placeholder:font-medium placeholder:text-gray-600 rounded-md m-2 focus:outline-none focus:ring focus:ring-[#56A6B1]" placeholder="Full Name" />
+                        <input type="text" name="mail" className="mail bg-[#F1F1F1] px-4 py-1 w-96 placeholder:font-medium placeholder:text-gray-600  rounded-md m-2 focus:outline-none focus:ring focus:ring-[#56A6B1]" placeholder="Email Address" />
                         <div className='flex flex-row password'>
-                            <input type="text" name="pass" className="pass bg-[#F1F1F1] px-4 py-1  placeholder:font-medium placeholder:text-gray-600 rounded-md m-2 focus:outline-none focus:ring focus:ring-[#56A6B1]" placeholder="Password" />
+                            <input type="text" name="pass" className="pass bg-[#F1F1F1] px-4 py-1 w-96 placeholder:font-medium placeholder:text-gray-600 rounded-md m-2 focus:outline-none focus:ring focus:ring-[#56A6B1]" placeholder="Password" />
                             <button className='passbtn'><AiFillEye className="btn-inside" /></button>
                         </div>
                         <div className='flex flex-row password'>
-                            <input type="password" name="cpass" className="cpass bg-[#F1F1F1] px-4 py-1 m-2  placeholder:font-medium placeholder:text-gray-600 rounded-md focus:outline-none focus:ring focus:ring-[#56A6B1]" placeholder="Confirm Password" />
+                            <input type="password" name="cpass" className="cpass bg-[#F1F1F1] px-4 py-1 m-2 w-96 placeholder:font-medium placeholder:text-gray-600 rounded-md focus:outline-none focus:ring focus:ring-[#56A6B1]" placeholder="Confirm Password" />
                             <button className='passbtn'><AiFillEye className='btn-inside' /></button>
                         </div>
-                        <div className="flex flex-row text-center m-2">
-                            <input type="checkbox" className=" w-3 m-1" />
+                        <div className="flex flex-row w-96 items-center text-center m-2">
+                            <input type="checkbox" className="mb-4" />
                             <p className="text-sm m-1">I have read and accepted the<span className="font-bold text-[#F96302]">Terms and Condition </span>to Devoct</p>
                         </div>
-                        <button className="cbtn my-6 bg-[#F96302] text-white font-bold py-1 px-1 rounded-md">Create Account</button>
-                        <p className='etext'><span>Or sign up with</span></p>
+                        <button className="cbtn my-4 ml-2 bg-[#F96302] text-white font-bold py-1 px-1 w-96 rounded-md">Create Account</button>
                     </div>
+                    <p className='etext'><span>Or sign up with</span></p>
+                    <div className='externalLinks text-center text-4xl text-blue-900'>
+                        <button className='m-5'><FaFacebookF /></button>
+                        <button className='m-5' ><FcGoogle /></button>
+                        <button className='m-5'><BsLinkedin /></button>
+                    </div>
+                    <p className='llogin text-center'>Already have an account? <a className='ml-2 text-[#F96302] font-bold underline' href="#">Sign In</a></p>
                 </div>
             </div>
-            <div className="right flex text-center flex-auto flex-col  bg-[#FDC8A6] w-auto">
-                <div className="content flex flex-col">
-                    <h2 className="bold">Welcome to Devoct</h2>
-                    <p className="w-42">Developing the best and efficient website, web applications and software application </p>
+
+            <div className="right bg-[#FDC8A6]">
+                <div className="text-center px-16 py-20 items-center">
+                    <div className="content flex-col justify-center">
+                        <h2 className="font-bold text-4xl">Welcome to Devoct</h2>
+                        <p className="w-34 text-lg font-medium my-2 mx-40">Developing the best and efficient website, web applications and software application </p>
+                        <img className="h-96 w-96 avatar" src={vector} alt="sign_emoj" />
+                    </div>
                 </div>
-                <img className="h-40 w-40" src={vector} alt="sign_emoj" />
             </div>
         </main>
     );
