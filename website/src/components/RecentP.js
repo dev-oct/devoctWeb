72de21730a5d19da53497fb59c0f3472c35c119b
dev-oct <chintantripathi1310@gmail.com>
2023-03-17 15:25:51 +0530

    project components are created

---
+++ website/src/components/RecentP.js
@@ -0,0 +1,14 @@
+const RecentP = () => {
+    return (
+        <div>
+            <div>
+                <h1>Recent Project</h1>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+}
+
+export default RecentP;
---
+++ website/src/components/TrendingP.js
@@ -0,0 +1,14 @@
+const TrendingP = () => {
+    return (
+        <div>
+            <div>
+                <h1>Trending Project</h1>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+}
+
+export default TrendingP;
